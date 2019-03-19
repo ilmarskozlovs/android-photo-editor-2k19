16eb98199d1052b80ff63a4dc0a3937233511fe5
iSardor <sardor0194@gmail.com>
2019-03-19 12:21:13 +0200

    Привет всем

--- app/src/main/java/com/example/android_photo_editor_2k19/MainActivity.java
+++ app/src/main/java/com/example/android_photo_editor_2k19/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.android_photo_editor_2k19;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public String helloTeam(){
