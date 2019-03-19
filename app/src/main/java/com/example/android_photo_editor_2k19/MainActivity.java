09a284af5c46b876e463a031518bdf5e69c410fb
Rihards Dzerkals <rih.dze@gmail.com>
2019-03-19 12:18:19 +0200

    MALADEC

--- app/src/main/java/com/example/android_photo_editor_2k19/MainActivity.java
+++ app/src/main/java/com/example/android_photo_editor_2k19/MainActivity.java
@@ -14,4 +14,8 @@ public class MainActivity extends AppCompatActivity {
     public String helloTeam(){
         return "hello team!";
     }
+    
+    public String asdasd(){
+        return "MALADEC";
+    }
 }
