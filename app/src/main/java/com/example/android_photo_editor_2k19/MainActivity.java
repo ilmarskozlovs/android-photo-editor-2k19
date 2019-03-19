9118f02f31a2483f75db8bf12133a40ebc39aa29
Rihards Dzerkals <rih.dze@gmail.com>
2019-03-19 12:28:37 +0200

    GAMEISHARD

--- app/src/main/java/com/example/android_photo_editor_2k19/MainActivity.java
+++ app/src/main/java/com/example/android_photo_editor_2k19/MainActivity.java
@@ -27,4 +27,8 @@ public class MainActivity extends AppCompatActivity {
         return maladec.equals("MALADEC");
 
     }
+    
+    public String thisIsTooHard(){
+        return "bad choice :(";
+    }
 }
