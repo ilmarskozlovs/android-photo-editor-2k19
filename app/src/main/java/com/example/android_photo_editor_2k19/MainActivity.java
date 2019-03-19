c8867a316ea2e389ae8035143d29efb720a80c97
Ilmars Kozlovs <ilmars.kozlovs@inbox.lv>
2019-03-19 11:37:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android_photo_editor_2k19/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.android_photo_editor_2k19;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
