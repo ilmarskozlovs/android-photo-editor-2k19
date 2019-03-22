4a4e31d625c181cd10df9a98b280a50ef72a541c
Ilmars Kozlovs <ilmars.kozlovs@inbox.lv>
2019-03-22 14:31:05 +0200

    push new files

---
+++ app/src/main/java/com/example/android_photo_editor_2k19/Dependencies.java
@@ -0,0 +1,31 @@
+package com.example.android_photo_editor_2k19;
+
+public class Dependencies {
+//    dependencies {
+//        implementation fileTree(dir: 'libs', include: ['*.jar'])
+//
+//        implementation 'com.android.support.constraint:constraint-layout:1.1.3'
+//        implementation 'com.android.support:support-v4:26.0.2'
+//        testImplementation 'junit:junit:4.12'
+//
+//        androidTestImplementation 'com.android.support.test.espresso:espresso-core:3.0.2'
+//
+//        implementation 'info.androidhive:imagefilters:1.0.7'
+//
+//        // butter knife
+//        implementation 'com.jakewharton:butterknife:8.8.1'
+//        annotationProcessor 'com.jakewharton:butterknife-compiler:8.8.1'
+//
+//        // dexter M permissions
+//        implementation 'com.karumi:dexter:4.1.0'
+////    implementation 'com.android.support:appcompat-v7:27.0.2'
+//        implementation 'com.android.support:recyclerview-v7:26.0.2'
+//
+//        // ...
+//
+//        implementation 'info.androidhive:imagefilters:1.0.7'
+//
+//        implementation 'com.github.bumptech.glide:glide:4.9.0'
+//        annotationProcessor 'com.github.bumptech.glide:compiler:4.9.0'
+//    }
+}
